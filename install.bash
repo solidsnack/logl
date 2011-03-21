c012c8d49106b07833acc5dd41843b90d3984299
Jason Dusek <jason.dusek@gmail.com>
2011-03-21 06:03:51 +0000

    Does not work on Ubuntu.

--- install.bash
+++ install.bash
@@ -4,5 +4,5 @@ set -o errexit -o nounset -o pipefail
 debs=( libev-dev libgnutls-dev )
 
 aptitude install --assume-yes "${debs[@]}"
-cabal install snap-server -flibev -fgnutls
+cabal install snap-server -fgnutls #-flibev
 
