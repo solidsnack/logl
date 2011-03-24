4ddf20d8dff19cf1539d53f78a4c2626b41ed801
Jason Dusek <jason.dusek@gmail.com>
2011-03-24 11:21:02 +0000

    Builds.

--- Language/LogL/Interpreter.hs
+++ Language/LogL/Interpreter.hs
@@ -2,62 +2,81 @@
            , EmptyDataDecls
            , StandaloneDeriving
            , GADTs
+           , RecordWildCards
+           , TypeSynonymInstances
   #-}
 module Language.LogL.Interpreter where
 
 import Control.Applicative
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Control.Exception
+import Control.Monad
+import Data.ByteString.Char8 (ByteString)
+import Data.Time.Clock
+import Data.Word
+import System.IO.Error
 
 import qualified Database.SQLite3 as SQLite3
 
+import qualified Language.LogL.Pickle as Pickle
 import Language.LogL.Syntax
 
 
---  Responses should provide us enough information to correlate a user and
---  their actions.
+data Envelope i t where
+  Envelope :: (Interpreter i) ---------------------------------------------
+           => !UTCTime -> !UTCTime -> Info i -> Result t -> Envelope i t
 
-
-data Response i t where
-  OK          :: (Interpeter i) => !UTCTime -> Info i -> !t -> Response i t
-  ERROR       :: (Interpeter i) => !UTCTime -> Info i -> Response i t
-  TIMEOUT     :: (Interpeter i) => !UTCTime -> Info i -> Response i t
+data Result t                =  OK !t | ERROR | TIMEOUT
 
 
 class Interpreter backend where
-  Info backend              ::  *
-  Spec backend              ::  *
+  type Info backend         ::  *
+  type Spec backend         ::  *
   init                      ::  Spec backend -> IO backend
   shutdown                  ::  backend -> IO ()
-  run :: forall t. backend -> LogL t -> IO (Result backend t)
+  run :: forall t. backend -> LogL t -> IO (Envelope backend t)
 
 
 data SQLite = SQLite { wait :: Word16, db :: SQLite3.Database,
                        path :: String, lock :: MVar ()         }
-instance Backend SQLite where
-  type Info SQLite           =  (String, SQLite3.Error)
+instance Interpreter SQLite where
+  type Info SQLite           =  ByteString
   type Spec SQLite           =  (Word16, String)
-  init wait path             =  do
-    db                      <-  SQLite3.open s
+  init (wait, path)          =  do
+    db                      <-  SQLite3.open path
     lock                    <-  newMVar ()
-    return (SQLite wait path db lock)
-  shutdown SQLite{..}        =  withMVar (SQLite3.close db)
-  run SQLite{..} q           =  withMVar lock . forkIO . timeout info wait $ do
-    undefined -- query the database
+    return (SQLite wait db path lock)
+  shutdown SQLite{..}        =  withMVar' lock (SQLite3.close db)
+  run SQLite{..} q           =  withMVar' lock $ do
+    start                   <-  getCurrentTime
+    result                  <-  tryJust check_exc . timeout wait $ do
+                                  SQLite3.prepare db sql
+                                  return ERROR
+    let (msg, val)           =  case result of
+                                  Left msg  -> (Pickle.o msg, ERROR)
+                                  Right val -> ("", val)
+    stop                    <-  getCurrentTime
+    return $ Envelope start stop msg val
+   where
+    sql                      =  undefined
+    check_exc exc = (guard . isUserError) exc >> Just (ioeGetErrorString exc)
 
 
-timeout :: (Interpreter i) ----------------------------------------------
-        => Word16 -> Info i -> IO (Response i t) -> IO (Response i t)
-timeout info w io            =  do
-  start                     <-  getCurrentTime
+timeout                     ::  Word16 -> IO (Result t) -> IO (Result t)
+timeout w io                 =  do
   output                    <-  newEmptyMVar
   alarm                     <-  newEmptyMVar
   worker                    <-  forkIO $ do
                                   res    <- io
-                                  ontime <- tryPutMvar output res
+                                  ontime <- tryPutMVar output res
                                   when ontime (killThread =<< takeMVar alarm)
   (putMVar alarm =<<) . forkIO $ do
                           threadDelay (fromIntegral w)
-                          res      <- TIMEOUT info start <$> getCurrentTime
-                          overtime <- tryPutMVar output res
+                          overtime <- tryPutMVar output TIMEOUT
                           when overtime (killThread worker)
   takeMVar output
 
+
+withMVar'                   ::  MVar t' -> IO t -> IO t
+withMVar' mvar               =  withMVar mvar . const
