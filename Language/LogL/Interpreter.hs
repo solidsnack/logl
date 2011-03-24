7d723dcbadfb534081dc6a8c00ae150e6602d128
Jason Dusek <jason.dusek@gmail.com>
2011-03-24 20:15:25 +0000

    Refactoring, implementation of table create.

--- Language/LogL/Interpreter.hs
+++ Language/LogL/Interpreter.hs
@@ -9,10 +9,10 @@ module Language.LogL.Interpreter where
 
 import Control.Applicative
 import Control.Concurrent
-import Control.Concurrent.MVar
 import Control.Exception
 import Control.Monad
 import Data.ByteString.Char8 (ByteString)
+import Data.List
 import Data.Time.Clock
 import Data.Word
 import System.IO.Error
@@ -27,57 +27,91 @@ data Envelope i t where
   Envelope :: (Interpreter i) ---------------------------------------------
            => !UTCTime -> !UTCTime -> Info i -> Result t -> Envelope i t
 
-data Result t                =  OK !t | ERROR | TIMEOUT
+data Result t                =  OK !t | ERROR
 
 
 class Interpreter backend where
   type Info backend         ::  *
   type Spec backend         ::  *
-  init                      ::  Spec backend -> IO backend
-  shutdown                  ::  backend -> IO ()
+  start                     ::  Spec backend -> IO backend
+  stop                      ::  backend -> IO ()
   run :: forall t. backend -> LogL t -> IO (Envelope backend t)
 
 
-data SQLite = SQLite { wait :: Word16, db :: SQLite3.Database,
-                       path :: String, lock :: MVar ()         }
+data SQLite = SQLite { db :: SQLite3.Database, table :: String,
+                       path :: String, lock :: MVar ()          }
 instance Interpreter SQLite where
   type Info SQLite           =  ByteString
-  type Spec SQLite           =  (Word16, String)
-  init (wait, path)          =  do
+  type Spec SQLite           =  (String, String)
+  start (table, path)        =  do
     db                      <-  SQLite3.open path
-    lock                    <-  newMVar ()
-    return (SQLite wait db path lock)
-  shutdown SQLite{..}        =  withMVar' lock (SQLite3.close db)
-  run SQLite{..} q           =  withMVar' lock $ do
-    start                   <-  getCurrentTime
-    result                  <-  tryJust check_exc . timeout wait $ do
-                                  SQLite3.prepare db sql
+    stmt                    <-  SQLite3.prepare db (sqlite_table_stmt table)
+    _                       <-  SQLite3.step stmt
+    SQLite3.finalize stmt
+    SQLite db table path <$> newMVar ()
+  stop SQLite{..}            =  withMVar' lock (SQLite3.close db)
+  run SQLite{..} q           =  withMVar' lock . envelope $ do
+    result                  <-  tryJust check_exc $ do
+                                  stmt <- SQLite3.prepare db sql
                                   return ERROR
-    let (msg, val)           =  case result of
-                                  Left msg  -> (Pickle.o msg, ERROR)
-                                  Right val -> ("", val)
-    stop                    <-  getCurrentTime
-    return $ Envelope start stop msg val
+    return $ case result of
+      Left msg              ->  (Pickle.o msg, ERROR)
+      Right val             ->  ("", val)
    where
     sql                      =  undefined
     check_exc exc = (guard . isUserError) exc >> Just (ioeGetErrorString exc)
 
+sqlite_table_stmt name = unlines [ "CREATE TABLE IF NOT EXISTS " ++ name
+                                 , "  ( " ++ joined_schema ++ " );"
+                                 , "CREATE INDEX IF NOT EXISTS ON " ++ name
+                                 , "  ( user_timestamp );"
+                                 , "CREATE INDEX IF NOT EXISTS ON " ++ name
+                                 , "  ( timestamp );" ]
+ where
+  joined_schema              =  intercalate "\n  , " sqlite_schema
 
-timeout                     ::  Word16 -> IO (Result t) -> IO (Result t)
+sqlite_schema                =  [ "uuid           CHAR(36) PRIMARY KEY"
+                                , "user_timestamp VARCHAR(40) NOT NULL"
+                                , "timestamp      VARCHAR(40) NOT NULL"
+                                , "data           BLOB NOT NULL" ]
+
+
+data Timeout i               =  Timeout { wait :: Word16, worker :: i }
+data TimeoutInfo i           =  TIMEOUT | (Interpreter i) => COMPLETED (Info i)
+instance (Interpreter i) => Interpreter (Timeout i) where
+  type Info (Timeout i)      =  TimeoutInfo i
+  type Spec (Timeout i)      =  (Word16, Spec i)
+  start (wait, spec)         =  Timeout wait <$> start spec
+  stop Timeout{..}           =  stop worker
+  run Timeout{..} q          =  envelope $ do
+    res                     <-  timeout wait (run worker q)
+    return $ case res of
+      Just (Envelope _ _ info val) -> (COMPLETED info, val)
+      Nothing               ->  (TIMEOUT, ERROR)
+
+timeout                     ::  Word16 -> IO t -> IO (Maybe t)
 timeout w io                 =  do
   output                    <-  newEmptyMVar
   alarm                     <-  newEmptyMVar
   worker                    <-  forkIO $ do
                                   res    <- io
-                                  ontime <- tryPutMVar output res
+                                  ontime <- tryPutMVar output (Just res)
                                   when ontime (killThread =<< takeMVar alarm)
   (putMVar alarm =<<) . forkIO $ do
                           threadDelay (fromIntegral w)
-                          overtime <- tryPutMVar output TIMEOUT
+                          overtime <- tryPutMVar output Nothing
                           when overtime (killThread worker)
   takeMVar output
 
 
 withMVar'                   ::  MVar t' -> IO t -> IO t
 withMVar' mvar               =  withMVar mvar . const
+
+
+envelope :: (Interpreter i) => IO (Info i, Result t) -> IO (Envelope i t)
+envelope io                  =  do
+    start                   <-  getCurrentTime
+    (msg, val)              <-  io
+    stop                    <-  getCurrentTime
+    return $ Envelope start stop msg val
 
