94e929788ece4342207a944bf6b476a470541332
Jason Dusek <jason.dusek@gmail.com>
2011-03-26 08:04:53 +0000

    Add SQLite; implement log destruction.

--- sql/postgres_table_setup.sql
+++ sql/postgres_table_setup.sql
@@ -1,4 +1,5 @@
 
+--  idempotent setup
 DROP FUNCTION IF EXISTS "Log/L,v2011-03-25.setup"() CASCADE;
 CREATE OR REPLACE FUNCTION "Log/L,v2011-03-25.setup"()
 RETURNS SETOF text AS $$
@@ -16,11 +17,10 @@ BEGIN
     CREATE TABLE   "Log/L,v2011-03-25".logs
       ( uuid        uuid PRIMARY KEY
       , time        timestamp with time zone NOT NULL
+      , destroy     boolean NOT NULL
       , tag         bytea CHECK (length(tag) <= 128) NOT NULL ); 
-    CREATE INDEX   "logs/tag"
-              ON   "Log/L,v2011-03-25".logs (tag);
-    CREATE INDEX   "logs/time"
-              ON   "Log/L,v2011-03-25".logs (time);
+    CREATE INDEX   "logs/tag" ON "Log/L,v2011-03-25".logs (tag);
+    CREATE INDEX   "logs/time" ON "Log/L,v2011-03-25".logs (time);
     RETURN NEXT    'Log/L,v2011-03-25.logs';
   END IF;
   IF NOT EXISTS ( SELECT 1 FROM information_schema.tables
@@ -29,15 +29,14 @@ BEGIN
   THEN
     CREATE TABLE   "Log/L,v2011-03-25".entries
       ( uuid        uuid NOT NULL
-      , log         uuid NOT NULL
+      , log         uuid REFERENCES "Log/L,v2011-03-25".logs (uuid) NOT NULL
       , user_time   timestamp with time zone NOT NULL
       , time        timestamp with time zone NOT NULL
       , data        bytea NOT NULL
       ,             PRIMARY KEY (uuid, log)           ); 
     CREATE INDEX   "entries/log,user_time"
               ON   "Log/L,v2011-03-25".entries (log, user_time);
-    CREATE INDEX   "entries/time"
-              ON   "Log/L,v2011-03-25".entries (time);
+    CREATE INDEX   "entries/time" ON "Log/L,v2011-03-25".entries (time);
     RETURN NEXT    'Log/L,v2011-03-25.entries';
   END IF;
 END;
---
+++ sql/sqlite.ddl.sql
@@ -0,0 +1,17 @@
+
+
+CREATE TABLE IF NOT EXISTS logs
+  ( uuid            CHAR(36) PRIMARY KEY
+  , timestamp       CHAR(40) NOT NULL
+  , destroy         BOOLEAN
+  , tag             BLOB CHECK(length(tag) <= 128) NOT NULL ); 
+
+CREATE TABLE IF NOT EXISTS entries
+  ( uuid            CHAR(36)
+  , log             CHAR(36)
+  , user_timestamp  CHAR(40) NOT NULL
+  , timestamp       CHAR(40) NOT NULL
+  , data            BLOB NOT NULL
+  ,                 PRIMARY KEY (uuid, log) ); 
+
+
