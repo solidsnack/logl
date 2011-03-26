ec6996593ce779cdce3459feeefebda6372ec16d
Jason Dusek <jason.dusek@gmail.com>
2011-03-26 08:05:55 +0000

    Comments to guide templating.

--- sql/sqlite.ddl.sql
+++ sql/sqlite.ddl.sql
@@ -1,11 +1,10 @@
 
-
+--  idempotent setup
 CREATE TABLE IF NOT EXISTS logs
   ( uuid            CHAR(36) PRIMARY KEY
   , timestamp       CHAR(40) NOT NULL
   , destroy         BOOLEAN
   , tag             BLOB CHECK(length(tag) <= 128) NOT NULL ); 
-
 CREATE TABLE IF NOT EXISTS entries
   ( uuid            CHAR(36)
   , log             CHAR(36)
