767f0ed822f30fec1452cae75176f24e7b0e5ddd
Jason Dusek <jason.dusek@gmail.com>
2011-03-31 05:26:37 +0000

    Vast simplification -- only entries.

--- Language/LogL/Syntax.hs
+++ Language/LogL/Syntax.hs
@@ -6,8 +6,6 @@
 module Language.LogL.Syntax where
 
 import Data.ByteString.Char8
-import Data.Set (Set)
-import Data.String
 import Data.Time.Clock
 import Data.Time.Format()
 
@@ -16,33 +14,23 @@ import qualified Language.LogL.UUID as UUID
 
 
 data LogL t where
-  Alloc                     ::  Tag -> LogL (ID Log)
-  Free                      ::  ID Log -> LogL ()
-  Append                    ::  ID Log -> UTCTime -> Tag -> ByteString
-                            ->  LogL (ID Entry)
-  SearchEntries :: ID Log -> (UTCTime, UTCTime) -> ID Entry -> Word8 -> Tag
-                -> LogL (Set (ID Entry))
-  SearchLogs                ::  Set (ID Log) -> Tag -> LogL (Set (ID Log))
+  Post                      ::  !ID -> !Message -> LogL ID
+  Free                      ::  !ID -> LogL ()
+  Leaves                    ::  !ID -> LogL [Entry]
+  Chain                     ::  !ID -> !ID -> LogL [Entry]
 
-
-data Log                     =  Log !(ID Log) !UTCTime !Tag
-deriving instance Eq Log
-deriving instance Ord Log
-deriving instance Show Log
-
-
-data Entry                   =  Entry !(ID Entry) !(ID Log) !UTCTime
-                                      !UTCTime !Tag !ByteString
+data Entry                   =  Entry !ID !ID !UTCTime !Tag !UTCTime
+                                      !ByteString
 deriving instance Eq Entry
 deriving instance Ord Entry
 deriving instance Show Entry
 
-newtype ID t                 =  ID UUID.V1
-deriving instance Eq (ID t)
-deriving instance Ord (ID t)
-deriving instance Show (ID t)
+newtype ID                   =  ID UUID.V1
+deriving instance Eq ID
+deriving instance Ord ID
+deriving instance Show ID
 
-data Message                 =  Message !UTCTime !Tag !ByteString
+data Message                 =  Message !Tag !UTCTime !ByteString
 deriving instance Eq Message
 deriving instance Ord Message
 deriving instance Show Message
