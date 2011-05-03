8e588afe4a9129a68f6c854788be79f683dbb74b
Jason Dusek <jason.dusek@gmail.com>
2011-05-03 09:12:08 +0000

    Added date headers, handled some errors more nicely.

--- Language/LogL/Server.hs
+++ Language/LogL/Server.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE OverloadedStrings
            , RecordWildCards
+           , TupleSections
   #-}
 
 module Language.LogL.Server where
 
+import Prelude hiding (head)
+import Control.Applicative
 import Control.Monad
-import Data.ByteString.Char8()
+import Control.Monad.Trans
+import Data.ByteString.Char8 (pack, ByteString)
+import Data.Monoid
+import System.Time
+import System.Locale
 
+import Data.Enumerator hiding (head)
 import qualified Network.HTTP.Types as Web
 import qualified Network.Wai as Web
 import qualified Network.Wai.Handler.Warp as Web
@@ -17,26 +25,55 @@ wai                         ::  Web.Application
 wai Web.Request{..}          =  methodCheck
  where
   methodCheck                =  case (Web.parseMethod requestMethod) of
-    Right Web.GET           ->  if pathInfo /= [] then return badPath
-                                                  else return get
-    Right Web.POST          ->  if pathInfo /= [] then return badPath
-                                                  else return post
-    Right _                 ->  return unhandledMethod
-    Left _                  ->  return unknownMethod
+    Right Web.GET           ->  if pathInfo /= [] then badPath
+                                                  else get
+    Right Web.POST          ->  if pathInfo /= [] then badPath
+                                                  else post
+    Right Web.HEAD          ->  if pathInfo /= [] then badPath
+                                                  else head
+    Right _                 ->  unhandledMethod
+    Left _                  ->  unknownMethod
 
 
-badPath                      =  http400 [] "error: bad path\n"
-unhandledMethod              =  http400 [] "error: unhandled method\n"
-unknownMethod                =  http400 [] "error: unknown method\n"
+badPath                      =  http404 []
+unhandledMethod              =  http405 [("Allow", "POST, GET, HEAD")]
+unknownMethod                =  http400 []
 
-get                          =  http200 [] "good: GET\n"
-post                         =  http200 [] "good: POST\n"
+get                          =  http200 [contentHTML] ""
+post                         =  http200 [contentYAML] "good: POST\n"
+head                         =  http200 [contentHTML] ""
 
 
-http400 headers msg = Web.ResponseBuilder Web.status400 headers (blaze msg)
+http400 headers              =  response Web.status400 headers mempty
+http404 headers              =  response Web.status404 headers mempty
+http405 headers              =  response Web.status405 headers mempty
+http200 headers msg          =  response Web.status200 headers (blaze msg)
 
-http200 headers msg = Web.ResponseBuilder Web.status200 headers (blaze msg)
+
+response :: (MonadIO m) ----------------------------------------------------
+         => Web.Status -> [Web.Header] -> Blaze.Builder -> m Web.Response
+response status headers builder = do
+  date                      <-  liftIO date
+  return $ Web.ResponseBuilder status (date:headers) builder
 
 
 blaze                        =  Blaze.fromByteString
+
+
+contentHTML                  =  ("Content-Type", "text/html; charset=utf-8")
+contentYAML                  =  ("Content-Type", "text/yaml")
+
+date                         =  ("Date",) . pack <$> httpCurrentDate
+
+
+{-| This code borrowed directly from:
+
+      <http://hackage.haskell.org/package/hS3-0.5.1>
+
+ -}
+httpCurrentDate             ::  IO String
+httpCurrentDate              =  do
+  c                         <-  getClockTime
+  let utc_time               =  (toUTCTime c) {ctTZName = "GMT"}
+  return $ formatCalendarTime defaultTimeLocale rfc822DateFormat utc_time
 
