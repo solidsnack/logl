420fe07a9388362d181bdb881239e8671ca0eeb3
Jason Dusek <jason.dusek@gmail.com>
2011-08-16 12:23:15 +0000

    Error handling for backend init failure.

--- walkthrough/server.hs
+++ walkthrough/server.hs
@@ -1,9 +1,12 @@
 #!/usr/bin/env runhaskell
 {-# LANGUAGE ScopedTypeVariables
            , OverloadedStrings
+           , NoMonomorphismRestriction
   #-}
 
 import Control.Applicative
+import Control.Exception as Exception
+import System.Exit
 
 import Network.Wai.Handler.Warp
 
@@ -15,13 +18,19 @@ import Language.LogL.PG
 
 main                         =  do
   putStrLn "Starting backend..."
-  bk :: Sharded Postgres    <- start ((2,3), conninfos)
+  bk :: Sharded Postgres    <-  catch
+    (start ((2,3), conninfos))
+    (errOut "An error occured while initializing the backend.")
   putStrLn "Starting server..."
   serve 0xFF bk defaultSettings Nothing
+ where
+  catch :: IO t -> (Exception.SomeException -> IO t) -> IO t
+  catch                      =  Exception.catch
+  errOut m e                 =  putStrLn m >> print e >> exitFailure
 
 conninfos = conn <$> [ "logl0", "logl1", "logl2", "logl3", "logl4",
                        "logl5", "logl6", "logl7", "logl8", "logl9" ]
  where
-  conn dbname =    conninfo{ dbname=dbname, host="localhost",
-                             user="logl",   password="pwned" }
+  conn dbname                =  conninfo{ dbname=dbname, host="localhost",
+                                          user="logl",   password="pwned" }
 
